af4a36bf3e50f5dd39bd8c9132fa66180fda0434
joshjb84@hotmail.com <joshjb84@hotmail.com>
2017-01-15 18:09:05 +1100

    initial commit

---
+++ src/LightAnchor.Extensions.RunWeb/Options.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LightAnchor.Extensions.RunWeb
+{
+    public class Options
+    {
+        string[] OpenBrowserOptionAlias = { "-o", "--open" };
+        string[] PortNumberOptionAlias = { "-r", "--port" };
+
+        public Options(string[] args)
+        {
+            ShouldOpenBrowser = FindOptionIndex(args, OpenBrowserOptionAlias) >= 0;
+            SetPortNumberFromOption(args);
+            FindUnknownOptions(args);
+        }
+
+        private void SetPortNumberFromOption(string[] args)
+        {
+            var indexPortNumberArg = FindOptionIndex(args, PortNumberOptionAlias);
+
+            if (indexPortNumberArg >= 0)
+            {
+                if (indexPortNumberArg + 1 == args.Length)
+                    OptionErrors.Add("No port number provided");
+                else
+                {
+                    int portNumber;
+                    if (Int32.TryParse(args[indexPortNumberArg + 1], out portNumber))
+                    {
+                        PortNumber = portNumber.ToString();
+                    }
+                    else
+                        OptionErrors.Add("Invalid port number format");
+                }
+
+            }
+        }
+
+        private void FindUnknownOptions(string[] args)
+        {
+            bool lastArgWasPortNumber = false;
+            var knownArguments = OpenBrowserOptionAlias.Concat(PortNumberOptionAlias);
+            foreach (var arg in args)
+            {
+                if (!knownArguments.Contains(arg) && (!lastArgWasPortNumber || arg.StartsWith("-")))
+                {
+                    UnknownArgs.Add(arg);
+                }
+
+                lastArgWasPortNumber = PortNumberOptionAlias.Contains(arg);
+            }
+        }
+
+        private static int FindOptionIndex(string[] array, string[] values)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Array.IndexOf(values, array[i]) >= 0)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public bool ShouldOpenBrowser { get; private set; }
+
+        public string PortNumber { get; private set; } = "5001";
+
+        public List<string> UnknownArgs { get; set; } = new List<string>();
+
+        public List<string> OptionErrors { get; private set; } = new List<string>();
+    }
+}
---
+++ src/LightAnchor.Extensions.RunWeb/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightAnchor.Extensions.RunWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string DefaultURIEnvironmentVariable = "ASPNETCORE_URLS";
+
+            var options = new Options(args);
+            options.OptionErrors.ForEach(Console.Error.WriteLine);
+
+            var uri = $"http://localhost:{options.PortNumber}";
+            Environment.SetEnvironmentVariable(DefaultURIEnvironmentVariable, uri);
+
+            var process = StartDotNetRun(options.UnknownArgs);
+            StreamReader reader = process.StandardOutput;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (options.ShouldOpenBrowser && IsWebServerOnlineMessage(line))
+                {
+                    Browser.StartBrowser(uri);
+                }
+                Console.Out.WriteLine(line);
+            }
+        }
+
+        private static bool IsWebServerOnlineMessage(string line) => line.Contains("Now listening on");
+
+        private static Process StartDotNetRun(IEnumerable<string> argumentsForDotNetRun)
+        {
+            var process = new Process();
+
+            process.StartInfo.FileName = "dotnet.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.Arguments = $"run {String.Join(" ", argumentsForDotNetRun)}";
+            process.Start();
+
+            return process;
+        }
+    }
+}
